6a1dbe5ff8e7a1b3da4cb4ff9f832ae1b95b2ba9
Denis <dzarnitsyn@profit.com.ua>
2026-04-16 16:49:47 +0300

    fix

--- src/bot.js
+++ src/bot.js
@@ -2,10 +2,11 @@ const { Telegraf } = require('telegraf');
 const { showMenu } = require('./handlers/menu');
 const { explosionHandler } = require('./handlers/explosion');
 const { currencyHandler } = require('./handlers/currency');
-// Змініть імпорт тут:
-const { dotaHandler, dotaPlayerHandler } = require('./handlers/dota'); 
-const { meguminHandler } = require('./handlers/megumin');
 
+// 🔴 ДОДАЙТЕ dotaPlayerHandler
+const { dotaHandler, dotaPlayerHandler } = require('./handlers/dota');
+
+const { meguminHandler } = require('./handlers/megumin');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.catch((err, ctx) => {
@@ -17,11 +18,8 @@ bot.command('bot', showMenu);
 bot.action('EXPLOSION', explosionHandler);
 bot.action('CURRENCY', currencyHandler);
 bot.action('MEGUMIN', meguminHandler);
-
-// Кнопка загального меню Доти
 bot.action('DOTA', dotaHandler);
 
-// 👇 ДОДАЙТЕ ЦЕЙ РЯДОК: Слухач для конкретних гравців
 bot.action(/^DOTA_PLAYER_(\d+)$/, dotaPlayerHandler);
 
 module.exports = { bot };
